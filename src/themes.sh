8309cffa03f6958b4332723eea34a44e6639727d
Adib Hanna <adibhanna@gmail.com>
2025-05-27 08:33:55 -0500

    wip

--- src/themes.sh
+++ src/themes.sh
@@ -29,25 +29,25 @@ case $SELECTED_THEME in
 
 "soft")
   declare -A THEME=(
-    ["background"]="#32302f"
-    ["foreground"]="#d4be98"
-    ["black"]="#3c3836"
-    ["blue"]="#7daea3"
-    ["cyan"]="#89b482"
-    ["green"]="#a9b665"
-    ["magenta"]="#d3869b"
-    ["red"]="#ea6962"
-    ["white"]="#d4be98"
-    ["yellow"]="#e78a4e"
+    ["background"]="#f8f4d6"
+    ["foreground"]="#282828"
+    ["black"]="#f8f4d6"
+    ["blue"]="#076678"
+    ["cyan"]="#427b58"
+    ["green"]="#79740e"
+    ["magenta"]="#8f3f71"
+    ["red"]="#9d0006"
+    ["white"]="#3c3836"
+    ["yellow"]="#b57614"
 
-    ["bblack"]="#3c3836"
-    ["bblue"]="#7daea3"
-    ["bcyan"]="#89b482"
-    ["bgreen"]="#a9b665"
-    ["bmagenta"]="#d3869b"
-    ["bred"]="#ea6962"
-    ["bwhite"]="#a89984"
-    ["byellow"]="#e78a4e"
+    ["bblack"]="#9d8374"
+    ["bblue"]="#458588"
+    ["bcyan"]="#689d69"
+    ["bgreen"]="#98971a"
+    ["bmagenta"]="#b16186"
+    ["bred"]="#cc241d"
+    ["bwhite"]="#7c6f64"
+    ["byellow"]="#d79921"
   )
   ;;
 
